eb0909a06998e2307aacaf2537189d40a46369c7
Raizaen <31708305+Raizaen@users.noreply.github.com>
2017-10-16 11:20:53 +0200

    ...

---
+++ Exo10.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string palindromeCheck (string word) {
+  int wordLength = word.length();
+  
+  for (int i=0; i<wordLength/2; i++)
+  {
+    if (word[i] != word[wordLength-i-1]) {
+      return " non ";
+    }
+  }
+  return " ";
+}
+
+int main () {
+  string word;
+  cout << "Entrez un mot" << endl;
+  cin >> word;
+  cout << word << " est" << palindromeCheck(word) << "un palindrome" << endl;
+  
+  return 0;
+}
