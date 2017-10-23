e2c8fe7918e95cb7976364ceb9bc7f9653063a95
Raizaen <31708305+Raizaen@users.noreply.github.com>
2017-10-23 16:02:20 +0200

    Correction Voiture

--- Exo13.cpp
+++ Exo13.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -5,25 +6,29 @@
 
 int main()
 {
-  std::string sol, cara, guess;
+  std::string sol;
+  char guess;
   int cpt(0);
   std::cout << "Entrez votre mot mystere : " << std::endl;
   std::cin >> sol;
-  std::array<std::string,100> T;
-  std::array<std::string,100> V;
-  for(unsigned int i(0);i<sol.length();i++)
-  {
-    cara = sol[i];
-    T.back(cara);
-    V.back('_');
+  //std::array<std::string,100> V;
+  
+  std::string V = sol;
+  for(unsigned int i=0;i<V.length();i++){
+    V[i]='_';
   }
+
+  /*for(unsigned int i(0);i<sol.length();i++)
+  {
+    V[i]= "_";
+  }*/
   system("CLS");
   std::cout << "Mot Mystere : ";
   for(unsigned int i(0);i<sol.length();i++)
   {
     std::cout << V[i] << " ";
   }
-  while(T!=V){
+  while(sol!=V){
     if(cpt!=0) {
       std::cout << "Apres " << cpt << " essai(s), voici le mot mystere: ";
       for(unsigned int i(0);i<sol.length();i++)
@@ -37,7 +42,7 @@ int main()
     cpt++;
     for(unsigned int i(0);i<sol.length();i++)
     {
-      if(guess==T[i])
+      if(guess==sol[i])
         V[i] = guess;
     }
   }
